4dee192bdbccf2d39ed6f966f684833b94667fdf
AlexanderC <alexander.moldova@gmail.com>
2017-12-15 12:14:50 +0200

    Update crypto.py

--- web3/eth_utils/crypto.py
+++ web3/eth_utils/crypto.py
@@ -1,22 +1,22 @@
 from __future__ import absolute_import
 
-
 try:
     from sha3 import keccak_256
 except ImportError:
-    from ..keccak.CompactFIPS202 import SHA3_256 as keccak_256
-    #from hashlib import sha256 as keccak_256
+    try:
+        from hashlib import sha256 as keccak_256
+    except ImportError:
+        try:
+            from ..keccak.py_keccak import Sha3_256 as keccak_256
+        except ImportError:
+            from ..keccak.CompactFIPS202 import SHA3_256 as keccak_256
 
 from .string import (
     force_bytes,
 )
 
-
 def keccak(value):
-   # print keccak_256(force_bytes(value))
-    print "%s: %s " % (value, keccak_256(force_bytes(value)))
     return keccak_256(force_bytes(value))
 
-
 # ensure we have the *correct* hash function
-#assert keccak('') == b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"  # noqa: E501
+assert keccak('') == b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"  # noqa: E501
